3bbb9cf5f16da2ba68450fc67b24c5e84bef2bf5
Maurice Paguagan <ellison.paguagan@siteminder.com>
2024-04-25 10:46:30 +0800

    feat: create a package for the implemented functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,14 @@
-import Math
+import utils.*
 
 fun main() {
-    println("The factorial of 5 is: ${Math().getFactorial(5)}")
-    println("Is 6 a prime number? ${Math().isPrime(6)}")
-    println("The reverse of \"Hello World\" is: ${Math().reverseString("Hello World")}")
-    println("The max element in the array [4, 5, 6, 10, 1] is: ${Math().findMaxElement(arrayOf(4, 5, 6, 10, 1))}")
-    println("The fibonacci value at the 10th term is: ${Math().getFibonacci(10)}")
-    println("Is \"Was it a cat I saw\" a palindrome? ${Math().checkPalindrome(" Was it a cat I saw")}")
-    println("The sum of digits 3697 is: ${Math().getSumOfDigits(3697)}")
-    println("The GCD of 10 and 30 is: ${Math().getGCD(10, 30)}")
-    println("The number of vowels in the string \"Ellison Maurice\" are: ${Math().countVowels(" Ellison Maurice")}")
-    println("100°C in Fahrenheit is: ${Math().celsiusToFahrenheit(100.0)} °F")
+    println("The factorial of 5 is: ${getFactorial(5)}")
+    println("Is 6 a prime number? ${isPrime(6)}")
+    println("The reverse of \"Hello World\" is: ${reverseString("Hello World")}")
+    println("The max element in the array [4, 5, 6, 10, 1] is: ${findMaxElement(arrayOf(4, 5, 6, 10, 1))}")
+    println("The fibonacci value at the 10th term is: ${getFibonacci(10)}")
+    println("Is \"Was it a cat I saw\" a palindrome? ${checkPalindrome(" Was it a cat I saw")}")
+    println("The sum of digits 3697 is: ${getSumOfDigits(3697)}")
+    println("The GCD of 10 and 30 is: ${getGCD(10, 30)}")
+    println("The number of vowels in the string \"Ellison Maurice\" are: ${countVowels(" Ellison Maurice")}")
+    println("100°C in Fahrenheit is: ${celsiusToFahrenheit(100.0)} °F")
 }
